f1a802f86a93f052afd91864aafa2b9f42023c79
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-06 20:09:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import os
+import sys
 
 print("""
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -14,7 +15,7 @@ print("""
 nama_file_video = input("Masukkan nama file video (format MP4): ")
 
 if not os.path.exists(nama_file_video):
-    print(f"Kesalahan: File video '{nama_file_video}' tidak ditemukan.")
+    print(f"Kesalahan: File video '{nama_file_video}' tidak ditemukan.\n")
     sys.exit(1)
 
 nama_file_audio = input("Masukkan nama file audio (format MP3): ")
@@ -23,17 +24,26 @@ if os.path.exists(nama_file_audio):
     pilihan = input(f"File audio '{nama_file_audio}' sudah ada. Apakah Anda ingin menimpa file yang ada? (iya/tidak): ")
     
     if pilihan.lower() != 'iya':
-        print("Konversi dibatalkan.")
+        print("Konversi dibatalkan.\n")
         sys.exit(1)
 
 try:
-    print("\n[*] Sedang melakukan konversi...")
+    print("\n[*] Sedang melakukan konversi.")
     devnull = open(os.devnull, 'w')
     subprocess.run(['ffmpeg', '-i', nama_file_video, '-vn', '-acodec', 'libmp3lame', '-q:a', '4', nama_file_audio], stdout=devnull, stderr=devnull)
     devnull.close()
-    print(f"\nKonversi selesai: {nama_file_video} menjadi {nama_file_audio}")
+    print(f"[*] Konversi selesai.")
+    
+    folder_hasil = "hasil"
+    
+    if not os.path.exists(folder_hasil):
+        os.makedirs(folder_hasil)
+    
+    os.rename(nama_file_audio, os.path.join(folder_hasil, nama_file_audio))
+    
+    print(f"[*] Hasil konversi disimpan dalam folder '{folder_hasil}'.\n")
     sys.exit(0)
     
 except Exception as e:
-    print(f"Terjadi kesalahan: {e}")
+    print(f"Terjadi kesalahan: {e}\n")
     sys.exit(1)
