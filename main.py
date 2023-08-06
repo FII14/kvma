e4c23ecae4461c6b43d59bf596bf6eedd4e3be35
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-06 22:58:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import subprocess
 import os
 import sys
 
@@ -22,28 +21,26 @@ nama_file_audio = input("Masukkan nama file audio (format MP3): ")
 
 if os.path.exists(nama_file_audio):
     pilihan = input(f"File audio '{nama_file_audio}' sudah ada. Apakah Anda ingin menimpa file yang ada? (iya/tidak): ")
-    
+
     if pilihan.lower() != 'iya':
         print("Konversi dibatalkan.\n")
         sys.exit(1)
 
 try:
     print("\n[*] Sedang melakukan konversi.")
-    devnull = open(os.devnull, 'w')
-    subprocess.run(['ffmpeg', '-i', nama_file_video, '-vn', '-acodec', 'libmp3lame', '-q:a', '4', nama_file_audio], stdout=devnull, stderr=devnull)
-    devnull.close()
+    os.system(f"ffmpeg -i {nama_file_video} -vn -acodec libmp3lame -q:a 4 {nama_file_audio} > /dev/null 2>&1")
     print(f"[*] Konversi selesai.")
-    
+
     folder_hasil = "hasil_konversi"
-    
+
     if not os.path.exists(folder_hasil):
         os.makedirs(folder_hasil)
-    
+
     os.rename(nama_file_audio, os.path.join(folder_hasil, nama_file_audio))
-    
+
     print(f"[*] Hasil konversi disimpan dalam folder '{folder_hasil}'.\n")
     sys.exit(0)
-    
+
 except Exception as e:
     print(f"Terjadi kesalahan: {e}\n")
     sys.exit(1)
